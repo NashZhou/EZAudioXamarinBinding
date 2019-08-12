c93f379f8678aac6a460a1ec259bf4a42afa96ba
Nash <24758364+NashZhou@users.noreply.github.com>
2019-08-12 00:03:08 -0500

    Adding iOS binding

---
+++ FoldingTabBar/iOS/EXFoldingTabBar/EXFoldingTabBar/CustomTabBarController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using UIKit;
+using CoreGraphics;
+using Foundation;
+using FoldingTabBariOS;
+namespace EXFoldingTabBar
+{
+	public class CustomTabBarController : YALFoldingTabBarController, IYALTabBarDelegate
+	{
+		public CustomTabBarController()
+		{
+			//** Constants is not part of CVFoldingTabBar.iOS, look at this project to find the source **//
+			TabBarView.ExtraTabBarItemHeight = Constants.YALExtraTabBarItemsDefaultHeight;
+			TabBarView.OffsetForExtraTabBarItems = Constants.YALForExtraTabBarItemsDefaultOffset;
+
+			TabBarView.BackgroundColor = new UIColor(94f / 255f, 91f / 255f, 149f / 255f, 1); //** #5e5b95 **//
+																							  //TabBarView.BackgroundColor = UIColor.Clear; //** Make the background clear **//
+			TabBarView.TabBarColor = new UIColor(72f / 255f, 211f / 255f, 178f / 255f, 1); //** #48d3b2 **//
+
+			TabBarView.TabBarViewEdgeInsets = Constants.YALTabBarViewHDefaultEdgeInsets;
+			TabBarView.TabBarItemsEdgeInsets = Constants.YALTabBarViewItemsDefaultEdgeInsets;
+
+			//** Set the delegate for the YALFoldingTabBarController **//
+			TabBarView.WeakDelegate = this;
+		}
+
+		#region YALTabBarDelegate
+
+		[Export("tabBarWillExpand:")]
+		public void TabBarWillExpand(YALFoldingTabBar tabBar)
+		{
+			System.Diagnostics.Debug.WriteLine("The bar will expand!");
+		}
+
+		[Export("tabBarDidExpand:")]
+		public void TabBarDidExpand(YALFoldingTabBar tabBar)
+		{
+			System.Diagnostics.Debug.WriteLine("The bar expanded!");
+		}
+
+		[Export("tabBarWillCollapse:")]
+		public void TabBarWillCollapse(YALFoldingTabBar tabBar)
+		{
+			System.Diagnostics.Debug.WriteLine("The bar will collapse!");
+		}
+
+		[Export("tabBarDidCollapse:")]
+		public void TabBarDidCollapse(YALFoldingTabBar tabBar)
+		{
+			System.Diagnostics.Debug.WriteLine("The bar collapsed!");
+		}
+
+		#endregion
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			//** This class is used to show how the IYALTabBarDelegate works in a view controller **//
+			var first = new TabPageViewController();
+			first.View.BackgroundColor = UIColor.Red;
+			first.Title = "Tab One";
+			var firstNavigationController = new UINavigationController(first)
+			{
+				Title = "First"
+			};
+
+			var second = new UIViewController();
+			second.View.BackgroundColor = UIColor.Green;
+			second.Title = "Tab Two";
+			var secondNavigationController = new UINavigationController(second)
+			{
+				Title = "Second"
+			};
+
+			//** Make sure to define the view controllers for the tab bar view controller **//
+			ViewControllers = new UIViewController[]
+			{
+				firstNavigationController, secondNavigationController
+			};
+
+			//** The second parameter in YALTabBarItem will show up as a button on the left side when the first UIViewController is present **//
+			YALTabBarItem item1 = new YALTabBarItem(new UIImage("profile_icon"), new UIImage("settings_icon"), null);
+			YALTabBarItem item2 = new YALTabBarItem(new UIImage("search_icon"), null, null);
+
+			//** The next 3 required properties to be defined are LeftBarItems, RightBarItems, and CenterButtonImage **//
+
+			LeftBarItems = new YALTabBarItem[]
+			{
+				item1
+			};
+			RightBarItems = new YALTabBarItem[]
+			{
+				item2
+			};
+
+			CenterButtonImage = new UIImage("plus_icon");
+		}
+
+
+		public override void ViewWillLayoutSubviews()
+		{
+			//** Define the height needed for the FoldingTabBar to look correct **//
+			nfloat height = (IsiPhoneX()) ? 80f + this.View.GetBottomInset() : 80f;
+			var frame = TabBar.Frame;
+			frame.Size = new CGSize(frame.Size.Width, height);
+			frame.Y = View.Frame.Size.Height - height;
+			TabBar.Frame = frame;
+			base.ViewWillLayoutSubviews();
+		}
+
+		bool IsiPhoneX()
+		{
+			if (UIDevice.CurrentDevice.CheckSystemVersion(11, 0))
+			{
+				var window = UIApplication.SharedApplication.KeyWindow;
+				if (window.SafeAreaInsets.Bottom > 0.0)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ FoldingTabBar/iOS/FoldingTabBariOS/FoldingTabBariOS/Const.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using UIKit;
+using Foundation;
+using ObjCRuntime;
+
+namespace FoldingTabBariOS
+{
+	//partial class UIView_Insets
+	//{
+	//    public static nfloat GetBottomInsets()
+	//    {
+	//        return (null as UIView).GetBottomInset();
+	//    }
+	//}
+
+	//partial class TransactionWithAnimationsAndCompletion 
+	//{
+	//    static readonly IntPtr class_ptr = Class.GetHandle("CATransaction");
+	//}
+
+	//partial class YALTabBarViewAnimations
+	//{
+	//    static readonly IntPtr class_ptr = Class.GetHandle("CAAnimation");
+	//}
+
+	//public struct YALAnimationParameters
+	//{
+	//    public double BeginTime;
+	//    public double Duration;
+	//    public double FromValue;
+	//    public double ToValue;
+	//    public double Damping;
+	//    public double Velocity;
+	//}
+	//public struct YALAdditionalButtonsAnimationsParameters
+	//{
+	//    public YALAnimationParameters ScaleX;
+	//    public YALAnimationParameters ScaleY;
+	//    public YALAnimationParameters Rotation;
+	//    public YALAnimationParameters Bounce;
+	//}
+	//public struct YALCenterButtonAnimationsParameters
+	//{
+	//    public YALAnimationParameters Rotation;
+	//    public YALAnimationParameters Bounce;
+	//}
+	//public struct YALExtraTabBarItemViewAnimationParameters
+	//{
+	//    public double Duration;
+	//    public double Delay;
+	//    public float Damping;
+	//    public float Velocity;
+	//    public UIViewAnimationOptions Options;
+	//}
+	//public struct YALSelectedDotAnimationsParameters
+	//{
+	//    public YALAnimationParameters ScaleX;
+	//    public YALAnimationParameters ScaleY;
+	//}
+	// public static class Constants
+	// {
+	//     const float YALBottomSelectedDotDefaultSize = 4.0f;
+	//     const float YALBottomSelectedDotOffset = 16.0f;
+	//     const float YALTabBarViewDefaultHeight = 80.0f;
+	//     const float YALExtraTabBarItemsDefaultHeight = 48.0f;
+	//     const float YALForExtraTabBarItemsDefaultOffset = 15.0f;
+	//     static UIEdgeInsets YALTabBarViewHDefaultEdgeInsets = new UIEdgeInsets(10.0f, 14.0f, 10.0f, 14.0f);
+	//     static UIEdgeInsets YALTabBarViewItemsDefaultEdgeInsets = new UIEdgeInsets(0f, 0f, 0f, 0f);
+	//     static NSString YALCenterButtonExpandAnimation = new NSString("CENTER_BUTTON_EXPAND_ANIMATION");
+	//     static NSString YALCenterButtonCollapseAnimation = new NSString("CENTER_BUTTON_COLLAPSE_ANIMATION");
+	//     static NSString YALAdditionalButtonsAnimation = new NSString("ADDITIONAL_BUTTONS_ANIMATION");
+	//     static NSString YALTabBarExpandAnimation = new NSString("TABBAR_EXPAND_ANIMATION");
+	//     static NSString YALTabBarExpandCollapseAnimation = new NSString("TABBAR_COLLAPSE_ANIMATION");
+	//     static NSString YALExtraLeftBarItemAnimation = new NSString("EXTRA_LEFT_BAR_ITEM_ANIMATION");
+	//     static NSString YALExtraRightBarItemAnimation = new NSString("EXTRA_RIGHT_BAR_ITEM_ANIMATION");
+	//     static double kYALExpandAnimationDuration = 1.0;
+	//     static float kDegreeToRadiansRatio = (float)Math.PI / 180f;
+
+	////     static YALAnimationParameters kYALBounceAnimationParameters = new YALAnimationParameters()
+	////     { 
+	////         duration = kYALExpandAnimationDuration * 2.0 / 3.0,
+	////         damping = 0.5,
+	////         velocity = 3.0
+	////     };
+
+	////     static YALAnimationParameters kYALExtraLeftTabBarItemAnimationParameters = new YALAnimationParameters()
+	////     { 
+	////         duration = kYALExpandAnimationDuration * 3.0 / 4.0,
+	////         damping = 0.74,
+	////         velocity = 1.2,
+	////         fromValue = 0,
+	////         toValue = Math.PI * 2.0 * 2.0
+	////     };
+
+	////     static YALAnimationParameters kYALExtraRightTabBarItemAnimationParameters = new YALAnimationParameters()
+	////     {
+	////duration = kYALExpandAnimationDuration * 3.0 / 4.0,
+	////damping = 0.74,
+	////velocity = 1.2,
+	////fromValue = 0,
+	////toValue = Math.PI * 2.0 * -2.0
+	//    //};
+
+	//    //static YALAnimationParameters kYALTabBarExpandAnimationParameters = new YALAnimationParameters()
+	//    //{
+	//    //    duration = kYALExpandAnimationDuration / 2.0,
+	//    //    damping = 0.5,
+	//    //    velocity = 0.6
+	//    //};
+
+	//    //static YALAnimationParameters kYALTabBarCollapseAnimationParameters = new YALAnimationParameters()
+	//    //{
+	//    //    duration = kYALExpandAnimationDuration * 0.6,
+	//    //    damping = 1,
+	//    //    velocity = 0.2
+	//    //};
+
+	//    //static YALCenterButtonAnimationsParameters kYALCenterButtonExpandAnimationParameters = new YALCenterButtonAnimationsParameters()
+	//    //{
+	//    //    rotation = new YALAnimationParameters()
+	//    //    {
+	//    //        duration = kYALExpandAnimationDuration / 4.0,
+	//    //        fromValue = 0.0,
+	//    //        toValue = Math.PI * 2.0 + 45.0 * kDegreeToRadiansRatio
+	//    //    },
+	//    //    bounce = new YALAnimationParameters()
+	//    //    {
+	//    //        beginTime = kYALExpandAnimationDuration / 4.0,
+	//    //        fromValue = 45.0 * kDegreeToRadiansRatio + Math.PI / 8.0,
+	//    //        toValue = 45.0 * kDegreeToRadiansRatio
+	//    //    }
+	//    //};
+
+	//    //static YALCenterButtonAnimationsParameters kYALCenterButtonCollapseAnimationParameters = new YALCenterButtonAnimationsParameters()
+	//    //{ 
+	//    //    rotation = new YALAnimationParameters()
+	//    //    {
+	//    //        duration = kYALExpandAnimationDuration / 4.0,
+	//    //        fromValue = 0.0,
+	//    //        toValue = 315.0 * kDegreeToRadiansRatio
+	//    //    },
+	//    //    bounce = new YALAnimationParameters()
+	//    //    {
+	//    //        beginTime = kYALExpandAnimationDuration / 4.0,
+	//    //        fromValue = Math.PI / 8.0,
+	//    //        toValue = 0.0
+	//    //    }
+	//    //};
+
+	//    //static YALSelectedDotAnimationsParameters kYALSelectedDotAnimationsParameters = new YALSelectedDotAnimationsParameters()
+	//    //{ 
+	//    //    scaleX = new YALAnimationParameters()
+	//    //    {
+	//    //        duration = kYALExpandAnimationDuration / 4.0,
+	//    //        fromValue = 0.0,
+	//    //        toValue = 1.0
+	//    //    },
+	//    //    scaleY = new YALAnimationParameters()
+	//    //    {
+	//    //        duration = kYALExpandAnimationDuration / 4.0,
+	//    //        fromValue = 0.0,
+	//    //        toValue = 1.0
+	//    //    }
+	//    //};
+
+	//    //static YALAdditionalButtonsAnimationsParameters kYALAdditionalButtonsAnimationsParameters = new YALAdditionalButtonsAnimationsParameters()
+	//    //{ 
+	//    //    scaleX = new YALAnimationParameters()
+	//    //    {
+	//    //        duration = kYALExpandAnimationDuration / 4.0,
+	//    //        fromValue = 0.0,
+	//    //        toValue = 1.0
+	//    //    },
+	//    //    scaleY = new YALAnimationParameters()
+	//    //    {
+	//    //        duration = kYALExpandAnimationDuration / 4.0,
+	//    //        fromValue = 0.0,
+	//    //        toValue = 1.0
+	//    //    },
+	//    //    rotation = new YALAnimationParameters()
+	//    //    {
+	//    //        duration = kYALExpandAnimationDuration / 4.0,
+	//    //        fromValue = 0.0,
+	//    //        toValue = Math.PI * 2.0 * 5.0
+	//    //    },
+	//    //    bounce = new YALAnimationParameters()
+	//    //    {
+	//    //        beginTime = kYALExpandAnimationDuration / 4.0,
+	//    //        fromValue = Math.PI / 8.0,
+	//    //        toValue = 0.0
+	//    //    }
+	//    //};
+
+	//    //static YALExtraTabBarItemViewAnimationParameters kYALShowExtraTabBarItemViewAnimationParameters = new YALExtraTabBarItemViewAnimationParameters()
+	//    //{
+	//    //    duration = kYALExpandAnimationDuration / 2.0,
+	//    //    damping = 0.5f
+	//    //};
+
+	//    //static YALExtraTabBarItemViewAnimationParameters kYALHideExtraTabBarItemViewAnimationParameters = new YALExtraTabBarItemViewAnimationParameters()
+	//    //{ 
+	//    //    duration = kYALExpandAnimationDuration / 8.0
+	//    //};
+	//}
+}
